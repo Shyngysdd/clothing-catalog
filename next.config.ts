09589904fad0d894b561ad9efdd086fe41936790
asankhanshyngys <255005@astanait.edu.kz>
2026-08-30 02:48:17 +0500

    Next

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.69"],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "6mb",
+    },
+  },
 };
 
 export default nextConfig;
